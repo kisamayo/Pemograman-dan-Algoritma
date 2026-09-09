6b9be7a2f0bdee4bf00a0b6843b56fc8ddb68cf2
nesv <luxlann2@gmail.com>
2026-09-09 11:14:44 +0700

    tugas 1

---
+++ Tugas Program/hitungvolumelimassegiempat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int s, t;
+float volume;
+
+int main()
+{
+    printf("Masukkan sisi : ");
+    scanf("%d", &s);
+    printf("Masukkan tinggi limas: ");
+    scanf("%d", &t);
+    volume = (1.0 / 3.0) * (s * s) * t;
+    printf("Volume limas adalah : %.2f", volume);
+    return 0;
+}
